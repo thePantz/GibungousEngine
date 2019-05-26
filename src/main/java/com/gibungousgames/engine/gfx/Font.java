1bf5f04eea1b7bb2b67de712f47b9c87f75785e9
Keith Tremorin <manwithnoname6@hotmail.com>
2019-05-25 23:43:09 -0500

    Implemented Font.java; To be used to load custom fonts from file

---
+++ src/main/java/com/gibungousgames/engine/gfx/Font.java
@@ -0,0 +1,50 @@
+package com.gibungousgames.engine.gfx;
+
+/**
+ * Loads a font from an image
+ * Expects the font image to have reserved colors to indicate the widths
+ * and offsets of each character
+ */
+public class Font {
+    private GameImage fontImage;
+    private int[] offsets;
+    private int[] widths;
+
+    public Font(String path)
+    {
+        fontImage = new GameImage(path);
+
+        //ToDo: allow fonts to support a different number of characters
+        offsets = new int[58];
+        widths = new int[58];
+        int unicode = 0;
+
+        for(int i = 0; i < fontImage.getWidth(); i++)
+        {
+            // Record start of character
+            if(fontImage.getPixels()[i] == 0xff0000ff)
+            {
+                offsets[unicode] = i;
+            }
+
+            // Record character width
+            if(fontImage.getPixels()[i] == 0xffffff00)
+            {
+                widths[unicode] = i - offsets[unicode];
+                unicode++;
+            }
+        }
+    }
+
+    public GameImage getFontImage() {
+        return fontImage;
+    }
+
+    public int[] getOffsets() {
+        return offsets;
+    }
+
+    public int[] getWidths() {
+        return widths;
+    }
+}
